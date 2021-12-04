a2b1885e33991768e5a6b2cb6ead7bad46ed8bf6
Tarmo Äijö <506537+tare@users.noreply.github.com>
2021-12-04 17:36:21 -0500

    Add zwo_workout tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,4 +1,4 @@
-"""unit tests for zwog.utils"""
+"""unit tests for zwog.utils."""
 import pytest
 from lark.exceptions import (UnexpectedCharacters,
                              UnexpectedEOF)
@@ -6,7 +6,7 @@ from lark.exceptions import (UnexpectedCharacters,
 from zwog.utils import ZWOG, WorkoutTransformer
 
 def test_zwog_grammar():
-    """Tests grammar."""
+    """Test grammar."""
     with pytest.raises(UnexpectedCharacters):
         ZWOG('x')
     with pytest.raises(UnexpectedCharacters):
@@ -33,7 +33,7 @@ def test_zwog_grammar():
         ZWOG(r'1f from 10 to 50% FTP')
 
 def test_duration():
-    """Tests duration."""
+    """Test duration."""
     assert WorkoutTransformer().duration([10,'h']) == 36000
     assert WorkoutTransformer().duration([10,'m']) == 600
     assert WorkoutTransformer().duration([10,'s']) == 10
@@ -42,41 +42,41 @@ def test_duration():
         WorkoutTransformer().duration([10,'x'])
 
 def test_durations():
-    """Tests durations"""
+    """Test durations."""
     assert WorkoutTransformer().durations([10,20]) == 30
 
 def test_steady_state():
-    """Tests steady_state."""
+    """Test steady_state."""
     assert WorkoutTransformer().steady_state([10,100.0]) == \
        {'duration':10,'power':100.0}
 
 def test_ramp():
-    """Tests ramp."""
+    """Test ramp."""
     assert WorkoutTransformer().ramp([10,[100.0,200.0]]) == \
         {'duration':10,'power':[100.0,200.0]}
 
 def test_power():
-    """Tests power."""
+    """Test power."""
     assert WorkoutTransformer().power([100.0]) == 100.0
     assert WorkoutTransformer().power([100.0,200.0]) == [100.0,200.0]
 
 def test_repeats():
-    """Tests repeats."""
+    """Test repeats."""
     assert WorkoutTransformer().repeats([1]) == ('repeats',1)
     assert WorkoutTransformer().repeats([2]) == ('repeats',2)
 
 def test_intervals():
-    """Tests intervals."""
+    """Test intervals."""
     assert WorkoutTransformer().intervals([1,2]) == ('intervals',[1,2])
 
 def test_block():
-    """Tests block."""
+    """Test block."""
     assert (WorkoutTransformer()
             .block([('intervals',[{'duration':600,'power':50.0}])])) == \
                 {'intervals':[{'duration':600,'power':50.0}]}
 
 def test_zwog_str():
-    """Tests __str__ (ZWOG)."""
+    """Test __str__ (ZWOG)."""
     assert str(ZWOG(r'55s @ 100% FTP')) == r'55s @ 100% FTP'
     assert str(ZWOG(r'1h60s @ 100% FTP')) == r'1h1m @ 100% FTP'
     assert str(ZWOG(r'1hrs60sec @ 100% FTP')) == r'1h1m @ 100% FTP'
@@ -97,14 +97,14 @@ def test_zwog_str():
         '3x 2m30s from 50 to 100% FTP, 2m @ 50% FTP\n5s @ 10% FTP'
 
 def test_tss():
-    """Tests tss (ZWOG)."""
+    """Test tss (ZWOG)."""
     assert ZWOG(r'60s @ 100% FTP').tss == 100/60
     assert ZWOG(r'60s from 0 to 100% FTP').tss == 100/60/2
     assert ZWOG(r'1h @ 66% FTP, 1h @ 100% FTP').tss == 166.0
     assert ZWOG(r'').tss == 0.0
 
 def test_json_workout():
-    """Tests json_workout (ZWOG)."""
+    """Test json_workout (ZWOG)."""
     assert ZWOG(r'60s @ 100% FTP').json_workout == \
         [{'intervals':
         [{'duration': 60, 'power': 100.0}]}]
@@ -124,10 +124,46 @@ def test_json_workout():
           'intervals': [{'duration': 50, 'power': [10.0, 100.0]}]},
          {'intervals': [{'duration': 7200, 'power': 90.0}]}]
 
-def test_tree_workout():
-    """Tests tree_workout (ZWOG)."""
-    pass
-
 def test_zwo_workout():
-    """Tests zwo_workout (ZWOG)."""
-    pass
+    """Test zwo_workout (ZWOG)."""
+    assert ZWOG(r'10m @ 50% FTP','John Dow','Cat1','SubCat1').zwo_workout == \
+        ('<workout_file><author>John Dow</author><name>Cat1</name>'
+         '<description>This workout was generated using ZWOG.\n\n'
+         '10m @ 50% FTP</description><sportType>bike</sportType>'
+         '<category>SubCat1</category><workout>'
+         '<SteadyState Duration="600" Power="0.5" />'
+         '</workout></workout_file>\n')
+
+    assert ZWOG((r'60s from 40 to 80% FTP 10m @ 80% FTP '
+                 r'10min from 80 to 70% FTP 1h from 70 to 50% FTP'),
+                 'John Dow','Cat1','SubCat1').zwo_workout == \
+                     ('<workout_file><author>John Dow</author><name>Cat1'
+                      '</name><description>This workout was generated using '
+                      'ZWOG.\n\n1m from 40 to 80% FTP\n10m @ 80% FTP\n10m '
+                      'from 80 to 70% FTP\n1h from 70 to 50% FTP'
+                      '</description><sportType>bike</sportType>'
+                      '<category>SubCat1</category><workout><Warmup '
+                      'Duration="60" PowerLow="0.4" PowerHigh="0.8" '
+                      '/><SteadyState Duration="600" Power="0.8" '
+                      '/><Ramp Duration="600" PowerLow="0.8" '
+                      'PowerHigh="0.7" /><Cooldown Duration="3600" '
+                      'PowerLow="0.7" PowerHigh="0.5" /></workout>'
+                      '</workout_file>\n')
+
+    assert ZWOG((r'1m @ 50% FTP 3x 5 min from 70 to 100% FTP, '
+                 r'5 min from 100 to 70% FTP 1m @ 50% FTP'),
+                'John Dow','Cat1','SubCat1').zwo_workout == \
+                    ('<workout_file><author>John Dow</author><name>Cat1</name>'
+                     '<description>This workout was generated using ZWOG.\n\n'
+                     '1m @ 50% FTP\n3x 5m from 70 to 100% FTP, 5m from 100 '
+                     'to 70% FTP\n1m @ 50% FTP</description><sportType>bike'
+                     '</sportType><category>SubCat1</category><workout>'
+                     '<SteadyState Duration="60" Power="0.5" /><Ramp '
+                     'Duration="300" PowerLow="0.7" PowerHigh="1.0" />'
+                     '<Ramp Duration="300" PowerLow="1.0" PowerHigh="0.7" '
+                     '/><Ramp Duration="300" PowerLow="0.7" PowerHigh="1.0" '
+                     '/><Ramp Duration="300" PowerLow="1.0" PowerHigh="0.7" '
+                     '/><Ramp Duration="300" PowerLow="0.7" PowerHigh="1.0" '
+                     '/><Ramp Duration="300" PowerLow="1.0" PowerHigh="0.7" '
+                     '/><SteadyState Duration="60" Power="0.5" /></workout>'
+                     '</workout_file>\n')
